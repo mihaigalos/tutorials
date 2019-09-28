2b426ee6bc89906e7a1a90742d900cd837f3a2b7
Mihai Galos <mihaigalos@gmail.com>
2019-09-28 22:31:20 +0200

    Add implementation for RMSProp

---
+++ rmsprop/src/main.rs
@@ -0,0 +1,20 @@
+mod rmsprop;
+use rmsprop::RMSProp;
+use rmsprop::Metadata;
+
+fn example_derrivative(x: f32) -> f32 {
+    4.0 * x.powf(3.0) - 9.0 * x.powf(2.0)
+}
+
+fn main() {
+    let metadata = Metadata {
+        start_x: 6.0,
+        step_size: 0.01,
+        precision: 0.00001,
+        max_iterations: 10000,
+        derrivative: example_derrivative,
+    };
+    let decay=0.9;
+    let (found, minimum_x) = RMSProp::run(metadata, decay);
+    println!("Solution : {}, minimum X found at: {}", found, minimum_x);
+}
