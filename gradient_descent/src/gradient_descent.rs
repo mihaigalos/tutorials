77eb3d2f4371673d95d31027dd66276bbc1df58a
Mihai Galos <mihaigalos@gmail.com>
2019-09-28 10:16:36 +0200

    Separate hard-coded derrivative to usage

--- gradient_descent/src/gradient_descent.rs
+++ gradient_descent/src/gradient_descent.rs
@@ -1,30 +1,25 @@
-// Gradient descent, implmented in Rust
 pub struct Metadata {
     pub next_x: f32,
     pub step_size: f32,
     pub precision: f32,
     pub max_iterations: i32,
+    pub derrivative: fn(f32) -> f32,
 }
 
 pub struct GrandientDescent {}
 
-impl GrandientDescent{
-    pub fn run(metadata : Metadata) {
+impl GrandientDescent {
+    pub fn run(metadata: Metadata) {
         let mut next = metadata.next_x;
-        for i in 0 .. metadata.max_iterations {
+        for i in 0..metadata.max_iterations {
             let current_x = next;
-            next = current_x - metadata.step_size * derrivative(&current_x);
+            next = current_x - metadata.step_size * (metadata.derrivative)(current_x);
 
             let step = next - current_x;
             println!("iteration: {}, current x: {}, step: {}", i, current_x, step);
             if step.abs() <= metadata.precision {
                 break;
             }
-        }    
+        }
     }
 }
-
-fn derrivative(x: &f32) -> f32 {
-    4.0 * x.powf(3.0) - 9.0 * x.powf(2.0)
-}
-
--- gradient_descent/src/main.rs
+++ gradient_descent/src/main.rs
@@ -1,8 +1,18 @@
 mod gradient_descent;
-use gradient_descent::Metadata;
 use gradient_descent::GrandientDescent;
+use gradient_descent::Metadata;
+
+fn example_derrivative(x: f32) -> f32 {
+    4.0 * x.powf(3.0) - 9.0 * x.powf(2.0)
+}
 
 fn main() {
-    let metadata = Metadata{next_x: 6.0, step_size: 0.01, precision: 0.00001, max_iterations: 10000};
+    let metadata = Metadata {
+        next_x: 6.0,
+        step_size: 0.01,
+        precision: 0.00001,
+        max_iterations: 10000,
+        derrivative: example_derrivative,
+    };
     GrandientDescent::run(metadata);
 }
