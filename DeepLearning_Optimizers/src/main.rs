77807d1e6dc1d75bd14dbc8f408d634f7beca53c
Mihai Galos <mihaigalos@gmail.com>
2019-10-07 23:38:38 +0200

    Not tested: multidimmensional derrivative

--- DeepLearning_Optimizers/src/main.rs
+++ DeepLearning_Optimizers/src/main.rs
@@ -1,13 +1,19 @@
 mod optimizers;
 use optimizers::ConfigMetadata;
 use optimizers::GrandientDescent;
-use optimizers::RMSProp;
-use optimizers::RMSPropMomentum;
+// use optimizers::RMSProp;
+// use optimizers::RMSPropMomentum;
 
-static VERBOSE: bool = false;
+static VERBOSE: bool = true;
 
-fn example_derrivative_x(x: f32) -> f32 {
-    4.0 * x.powf(3.0) - 9.0 * x.powf(2.0)
+// Z = 0.1*X**3-0.3*X*(Y**2)+0.9*X*Y
+// Dx= 0.3*x^2 - 0.3y^2 + 0.9y
+// Dy= -0.6y + 0.9x
+fn example_derrivative_x(x: Vec<f32>) -> f32 {
+    0.3 * x[0].powf(2.0) - 0.3 * x[1].powf(2.0) + 0.9*x[1]
+}
+fn example_derrivative_y(x: Vec<f32>) -> f32 {
+    -0.6*x[1] + 0.9*x[0]
 }
 
 fn epoch_callback_printer(epoch: i32, current_x: Vec<f32>, loss: f32, momentum: Option<f32>) {
@@ -18,16 +24,17 @@ fn epoch_callback_printer(epoch: i32, current_x: Vec<f32>, loss: f32, momentum: 
                 epoch, current_x, loss, x
             ),
             None => println!(
-                "Epoch: {}, current pos: {:?}, loss: {}",
-                epoch, current_x, loss
+                // "Epoch: {}, current pos: {:?}, loss: {}",
+                "{:?}",
+                current_x
             ),
         }
     }
 }
 
 fn main() {
-    let example_derrivatives: Vec<fn(f32) -> f32> = vec![example_derrivative_x];
-    let start_: Vec<f32> = vec![6.0];
+    let example_derrivatives: Vec<fn(Vec<f32>) -> f32> = vec![example_derrivative_x, example_derrivative_y];
+    let start_: Vec<f32> = vec![0.0,8.0];
 
     let config_metadata = ConfigMetadata {
         start: start_,
@@ -45,20 +52,20 @@ fn main() {
         found, minimum_x, epochs
     );
 
-    let rms_prop = RMSProp {};
-    let decay = 0.9;
-    let (found, minimum_x, epochs) = rms_prop.run(&config_metadata, decay);
-    println!(
-        "RMSProp: Solution: {:?}, minimum X found at: {:?}, epochs: {}",
-        found, minimum_x, epochs
-    );
+    // let rms_prop = RMSProp {};
+    // let decay = 0.9;
+    // let (found, minimum_x, epochs) = rms_prop.run(&config_metadata, decay);
+    // println!(
+    //     "RMSProp: Solution: {:?}, minimum X found at: {:?}, epochs: {}",
+    //     found, minimum_x, epochs
+    // );
 
-    let rms_prop_momentum = RMSPropMomentum {};
-    let decay = 0.9;
-    let mu = 0.9;
-    let (found, minimum_x, epochs) = rms_prop_momentum.run(&config_metadata, decay, mu);
-    println!(
-        "RMSProm with momentum: Solution: {:?}, minimum X found at: {:?}, epochs: {}",
-        found, minimum_x, epochs
-    );
+    // let rms_prop_momentum = RMSPropMomentum {};
+    // let decay = 0.9;
+    // let mu = 0.9;
+    // let (found, minimum_x, epochs) = rms_prop_momentum.run(&config_metadata, decay, mu);
+    // println!(
+    //     "RMSProm with momentum: Solution: {:?}, minimum X found at: {:?}, epochs: {}",
+    //     found, minimum_x, epochs
+    // );
 }
--- DeepLearning_Optimizers/src/optimizers.rs
+++ DeepLearning_Optimizers/src/optimizers.rs
@@ -1,3 +1,4 @@
+use std::process;
 #[derive(Debug)]
 pub enum OptimizerResult {
     Converged,
@@ -9,7 +10,7 @@ pub struct ConfigMetadata {
     pub step_size: f32,
     pub precision: f32,
     pub max_epochs: i32,
-    pub derrivatives: Vec<fn(f32) -> f32>,
+    pub derrivatives: Vec<fn(Vec<f32>) -> f32>,
     pub epoch_printer: fn(i32, Vec<f32>, f32, Option<f32>),
 }
 
@@ -57,6 +58,9 @@ impl GrandientDescent {
                     );
                 }
             }
+            // if epoch > 10{
+            //     process::exit(0x0100);
+            // }
         }
         non_converged(run_metadata)
     }
@@ -66,120 +70,122 @@ impl GrandientDescent {
     ) -> f32 {
         run_metadata.next[dimmension] = run_metadata.current[dimmension]
             - config_metadata.step_size
-                * (config_metadata.derrivatives[dimmension])(run_metadata.current[dimmension]);
+        
+                * (config_metadata.derrivatives[dimmension])(run_metadata.current.clone());
         let loss = run_metadata.next[dimmension] - run_metadata.current[dimmension];
         loss
+       
     }
 }
 
-pub struct RMSProp {}
+// pub struct RMSProp {}
 
-impl RMSProp {
-    pub fn run(
-        self, config_metadata: &ConfigMetadata, decay: f32,
-    ) -> (OptimizerResult, Vec<f32>, i32) {
-        let mut run_metadata = RunMetadata {
-            next: config_metadata.start.clone(),
-            current: Vec::with_capacity(0),
-            epochs: 0,
-        };
-        let mut dx_mean_sqr = 0.0;
-        for epoch in 0..config_metadata.max_epochs {
-            run_metadata.current = run_metadata.next.clone();
-            run_metadata.epochs += 1;
-            for dimmension in 0..config_metadata.derrivatives.len() {
-                let loss = self.implementation(
-                    &mut run_metadata,
-                    &config_metadata,
-                    dimmension,
-                    decay,
-                    &mut dx_mean_sqr,
-                );
+// impl RMSProp {
+//     pub fn run(
+//         self, config_metadata: &ConfigMetadata, decay: f32,
+//     ) -> (OptimizerResult, Vec<f32>, i32) {
+//         let mut run_metadata = RunMetadata {
+//             next: config_metadata.start.clone(),
+//             current: Vec::with_capacity(0),
+//             epochs: 0,
+//         };
+//         let mut dx_mean_sqr = 0.0;
+//         for epoch in 0..config_metadata.max_epochs {
+//             run_metadata.current = run_metadata.next.clone();
+//             run_metadata.epochs += 1;
+//             for dimmension in 0..config_metadata.derrivatives.len() {
+//                 let loss = self.implementation(
+//                     &mut run_metadata,
+//                     &config_metadata,
+//                     dimmension,
+//                     decay,
+//                     &mut dx_mean_sqr,
+//                 );
 
-                epoch_print(&run_metadata, &config_metadata, loss, epoch);
+//                 epoch_print(&run_metadata, &config_metadata, loss, epoch);
 
-                if loss.abs() <= config_metadata.precision {
-                    return (
-                        OptimizerResult::Converged,
-                        run_metadata.current,
-                        run_metadata.epochs,
-                    );
-                }
-            }
-        }
-        non_converged(run_metadata)
-    }
+//                 if loss.abs() <= config_metadata.precision {
+//                     return (
+//                         OptimizerResult::Converged,
+//                         run_metadata.current,
+//                         run_metadata.epochs,
+//                     );
+//                 }
+//             }
+//         }
+//         non_converged(run_metadata)
+//     }
 
-    fn implementation(
-        &self, run_metadata: &mut RunMetadata, config_metadata: &ConfigMetadata, dimmension: usize,
-        decay: f32, dx_mean_sqr: &mut f32,
-    ) -> f32 {
-        let epsilon = 0.00000001; // neccessary for numerical stability, avoid div with 0
-        let dx = (config_metadata.derrivatives[dimmension])(run_metadata.current[dimmension]);
-        *dx_mean_sqr = decay * (*dx_mean_sqr) + (1.0 - decay) * dx.powf(2.0);
-        run_metadata.next[dimmension] = run_metadata.current[dimmension]
-            - config_metadata.step_size * dx / (dx_mean_sqr.sqrt() + epsilon);
+//     fn implementation(
+//         &self, run_metadata: &mut RunMetadata, config_metadata: &ConfigMetadata, dimmension: usize,
+//         decay: f32, dx_mean_sqr: &mut f32,
+//     ) -> f32 {
+//         let epsilon = 0.00000001; // neccessary for numerical stability, avoid div with 0
+//         let dx = (config_metadata.derrivatives[dimmension])(run_metadata.current);
+//         *dx_mean_sqr = decay * (*dx_mean_sqr) + (1.0 - decay) * dx.powf(2.0);
+//         run_metadata.next[dimmension] = run_metadata.current[dimmension]
+//             - config_metadata.step_size * dx / (dx_mean_sqr.sqrt() + epsilon);
 
-        let loss = run_metadata.next[dimmension] - run_metadata.current[dimmension];
+//         let loss = run_metadata.next[dimmension] - run_metadata.current[dimmension];
 
-        loss
-    }
-}
+//         loss
+//     }
+// }
 
-pub struct RMSPropMomentum {}
+// pub struct RMSPropMomentum {}
 
-impl RMSPropMomentum {
-    pub fn run(
-        self, config_metadata: &ConfigMetadata, decay: f32, mu: f32,
-    ) -> (OptimizerResult, Vec<f32>, i32) {
-        let mut run_metadata = RunMetadata {
-            next: config_metadata.start.clone(),
-            current: Vec::with_capacity(0),
-            epochs: 0,
-        };
-        let mut dx_mean_sqr = 0.0;
-        let mut momentum = 0.0;
-        for epoch in 0..config_metadata.max_epochs {
-            run_metadata.current = run_metadata.next.clone();
-            run_metadata.epochs += 1;
-            for dimmension in 0..config_metadata.derrivatives.len() {
-                let loss = self.implementation(
-                    &mut run_metadata,
-                    &config_metadata,
-                    dimmension,
-                    decay,
-                    mu,
-                    &mut dx_mean_sqr,
-                    &mut momentum,
-                );
+// impl RMSPropMomentum {
+//     pub fn run(
+//         self, config_metadata: &ConfigMetadata, decay: f32, mu: f32,
+//     ) -> (OptimizerResult, Vec<f32>, i32) {
+//         let mut run_metadata = RunMetadata {
+//             next: config_metadata.start.clone(),
+//             current: Vec::with_capacity(0),
+//             epochs: 0,
+//         };
+//         let mut dx_mean_sqr = 0.0;
+//         let mut momentum = 0.0;
+//         for epoch in 0..config_metadata.max_epochs {
+//             run_metadata.current = run_metadata.next.clone();
+//             run_metadata.epochs += 1;
+//             for dimmension in 0..config_metadata.derrivatives.len() {
+//                 let loss = self.implementation(
+//                     &mut run_metadata,
+//                     &config_metadata,
+//                     dimmension,
+//                     decay,
+//                     mu,
+//                     &mut dx_mean_sqr,
+//                     &mut momentum,
+//                 );
 
-                epoch_print(&run_metadata, &config_metadata, loss, epoch);
+//                 epoch_print(&run_metadata, &config_metadata, loss, epoch);
 
-                if loss.abs() <= config_metadata.precision {
-                    return (
-                        OptimizerResult::Converged,
-                        run_metadata.current,
-                        run_metadata.epochs,
-                    );
-                }
-            }
-        }
-        non_converged(run_metadata)
-    }
+//                 if loss.abs() <= config_metadata.precision {
+//                     return (
+//                         OptimizerResult::Converged,
+//                         run_metadata.current,
+//                         run_metadata.epochs,
+//                     );
+//                 }
+//             }
+//         }
+//         non_converged(run_metadata)
+//     }
 
-    fn implementation(
-        &self, run_metadata: &mut RunMetadata, config_metadata: &ConfigMetadata, dimmension: usize,
-        decay: f32, mu: f32, dx_mean_sqr: &mut f32, momentum: &mut f32,
-    ) -> f32 {
-        let epsilon = 0.00000001; // neccessary for numerical stability, avoid div with 0
-        let dx = (config_metadata.derrivatives[dimmension])(run_metadata.current[dimmension]);
-        *dx_mean_sqr = decay * (*dx_mean_sqr) + (1.0 - decay) * dx.powf(2.0);
-        *momentum =
-            mu * (*momentum) + config_metadata.step_size * dx / (dx_mean_sqr.sqrt() + epsilon);
-        run_metadata.next[dimmension] = run_metadata.current[dimmension] - (*momentum);
+//     fn implementation(
+//         &self, run_metadata: &mut RunMetadata, config_metadata: &ConfigMetadata, dimmension: usize,
+//         decay: f32, mu: f32, dx_mean_sqr: &mut f32, momentum: &mut f32,
+//     ) -> f32 {
+//         let epsilon = 0.00000001; // neccessary for numerical stability, avoid div with 0
+//         let dx = (config_metadata.derrivatives[dimmension])(run_metadata.current);
+//         *dx_mean_sqr = decay * (*dx_mean_sqr) + (1.0 - decay) * dx.powf(2.0);
+//         *momentum =
+//             mu * (*momentum) + config_metadata.step_size * dx / (dx_mean_sqr.sqrt() + epsilon);
+//         run_metadata.next[dimmension] = run_metadata.current[dimmension] - (*momentum);
 
-        let loss = run_metadata.next[dimmension] - run_metadata.current[dimmension];
+//         let loss = run_metadata.next[dimmension] - run_metadata.current[dimmension];
 
-        loss
-    }
-}
+//         loss
+//     }
+// }
