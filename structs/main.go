fff3e14257898570bac9b0bd81907eb73f6ca715
Mihai Galos <mihaigalos@gmail.com>
2020-05-14 17:48:54 +0200

    Add implementation for structs

---
+++ structs/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
