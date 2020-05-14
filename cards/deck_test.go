eccfafb42fb02ce869dfc83438979af580e524db
Mihai Galos <mihaigalos@gmail.com>
2020-05-14 12:47:09 +0200

    Test first and last card

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,3 +8,19 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
+
+func TestNewDeckAceOfSpades(t *testing.T) {
+	d := newDeck()
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v.", d[0])
+	}
+}
+
+func TestNewDeckFourOfClubs(t *testing.T) {
+	d := newDeck()
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected first card of Four of Clubs, but got %v.", d[0])
+	}
+}
