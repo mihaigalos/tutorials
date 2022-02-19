02f4929b9e5cce8737f8d8c573346674ca6554ee
Mihai Galos <mihaigalos@gmail.com>
2022-02-19 13:47:37 +0100

    Vlang: split everything to tests

--- vlang/reflection/reflect_test.v
+++ vlang/reflection/reflect_test.v
@@ -1,5 +1,28 @@
 module main
 
+struct User {
+	name string
+	age  int
+}
+
+struct Location {
+	lat f32
+	lon f32
+}
+
+fn get_generic_data<T>(data T) string {
+	mut result := ''
+	$for field in T.fields {
+		result += data.$(field.name).str()
+		result += ' '
+	}
+	return result
+}
+
+fn print_generic<T>(data T) {
+	println(get_generic_data<T>(data))
+}
+
 fn test_users_when_typical() {
 	mut users := []User{cap: 100}
 	users << User{'Foo', 55}
