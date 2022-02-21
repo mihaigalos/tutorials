8e515e17db9b305dce209eba57607214eddf6612
Mihai Galos <mihaigalos@gmail.com>
2022-02-21 21:45:49 +0100

    Vlang: Custom Iterators: Implement for f64

--- vlang/iterators/custom_iterator_test.v
+++ vlang/iterators/custom_iterator_test.v
@@ -14,7 +14,7 @@ fn (mut iter SquareIterator<T>) next<T>() ?T {
 	return iter.arr[iter.idx] * iter.arr[iter.idx]
 }
 
-fn test_square_works_when_typical() {
+fn test_square_int_works_when_typical() {
 	expected := [1, 4, 9, 16, 25]
 	input := [1, 2, 3, 4, 5]
 	squares := SquareIterator<int>{
@@ -22,6 +22,21 @@ fn test_square_works_when_typical() {
 	}
 
 	mut actual := []int{}
+	for square in squares {
+		actual << square
+	}
+
+	assert actual == expected
+}
+
+fn test_square_f32_works_when_typical() {
+	expected := [2.25, 6.25, 12.25, 20.25, 30.25]
+	input := [1.5, 2.5, 3.5, 4.5, 5.5]
+	squares := SquareIterator<f64>{
+		arr: input
+	}
+
+	mut actual := []f64{}
 	for square in squares {
 		actual << square
 	}
